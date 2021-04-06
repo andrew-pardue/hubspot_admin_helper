6573473ab9fd8ea33385045757d0765e3934a543
Drew <andrewrpardue@gmail.com>
2021-04-06 09:25:57 -0400

    updated code to use Hubspot settings form, now works with 2.0-rc6

--- js/hubspot_submit_ajax.js
+++ js/hubspot_submit_ajax.js
@@ -5,12 +5,13 @@
         attach: function (context, settings) {
 
             $('form[method="post"]').submit(function(e) {
+                var btn = $(this).find("input[type=submit]:focus" );
                 e.preventDefault();
                 var data=$(this).serialize();
                 // Send the data via AJAX
                 $.ajax({
                     type: 'POST',
-                    url: '/hubspot-admin-helper/json',
+                    url: '/hubspot-admin-helper/json/json',
                     dataType: 'json',
                     success: function(data) {
                         // Just reload the page to refresh everything
